03f8cae7752170210519afbb308c9f4666fd973a
Akssasori <diniz3003@gmail.com>
2023-04-08 09:44:42 -0300

    adiconando prisma

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express'
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!dasdasdasd')
+  res.send('Hello World!dasdasdasd');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
